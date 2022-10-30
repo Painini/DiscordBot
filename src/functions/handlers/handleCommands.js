af575215c9ed410647273187e29399dcb831c0cc
Painini <72918879+Painini@users.noreply.github.com>
2022-10-30 15:55:28 +0100

    test script ready

--- src/functions/handlers/handleCommands.js
+++ src/functions/handlers/handleCommands.js
@@ -13,6 +13,7 @@ module.exports = (client) => {
         const command = require(`../../commands${folder}/${file}`);
         commands.set(command.data.name, command);
         commandArray.push(command.data.toJSON());
+        console.log(`Command: ${command.data.name} has been passed`)
       }
     }
   };
