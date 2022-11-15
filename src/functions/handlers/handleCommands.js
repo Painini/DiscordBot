5457f32e7cb0ac0c1868d9692d6b5932eba2c89a
Painini <tim.lofstedt@elev.ga.ntig.se>
2022-11-15 10:41:15 +0100

    added yellow text in terminal

--- src/functions/handlers/handleCommands.js
+++ src/functions/handlers/handleCommands.js
@@ -1,5 +1,6 @@
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
+const chalk = require('chalk');
 
 const fs = require("fs");
 
@@ -16,7 +17,7 @@ module.exports = (client) => {
         const command = require(`../../commands/${folder}/${file}`);
         commands.set(command.data.name, command);
         commandArray.push(command.data.toJSON());
-        console.log(`Command: ${command.data.name} has been passed`);
+        console.log(chalk.yellow(`Command: ${command.data.name} has been passed`));
       }
     }
 
