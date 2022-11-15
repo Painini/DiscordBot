2cc9d71415ad2a4b5dc9453e4f7283b092fd19b0
Painini <tim.lofstedt@elev.ga.ntig.se>
2022-11-15 10:43:02 +0100

    revert commit

--- src/functions/handlers/handleCommands.js
+++ src/functions/handlers/handleCommands.js
@@ -17,7 +17,7 @@ module.exports = (client) => {
         const command = require(`../../commands/${folder}/${file}`);
         commands.set(command.data.name, command);
         commandArray.push(command.data.toJSON());
-        console.log(chalk.yellow(`Command:`)  `${command.data.name} has been passed`);
+        console.log(chalk.yellow(`Command: ${command.data.name} has been passed`));
       }
     }
 
