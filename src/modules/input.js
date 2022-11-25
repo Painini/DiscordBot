4a48968fcfd85bb293d5e5055c7c57b3194572f0
Painini <tim.lofstedt@elev.ga.ntig.se>
2022-11-25 10:20:16 +0100

    updated comments

--- src/modules/input.js
+++ src/modules/input.js
@@ -13,3 +13,6 @@ module.exports = {givePromptAsync}
 //Save input from the terminal, send it to the bot 
 //Have an interval every ten seconds or so that closes the async function
 //AND runs a new async function that can take a new prompt input
+//This way I can always consistantly know that the async function
+//Will run on intervals. This solves the problem of it running 
+//only once or a thousand times a second.
